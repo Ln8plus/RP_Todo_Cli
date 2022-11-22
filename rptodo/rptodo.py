0392c2e08eeaffc12fcc7c32dfcb64042962b254
Ln8plus <akashdeadmau5@live.in>
2022-11-22 12:48:41 +0530

    - Added support for communicating with CLI. - Implemented classes to hold DB's responses & R/W data to it. - Connected above code to CLI.

---
+++ rptodo/rptodo.py
@@ -0,0 +1,13 @@
+from typing import Any, Dict, NamedTuple
+from pathlib import Path
+from rptodo.database import DatabaseHandler
+
+class CurrentTodo(NamedTuple):
+    todo: Dict[str, Any]
+    error: int
+
+
+class Todoer:
+    def __init__(self, db_path: Path) -> None:
+        self._db_handler = DatabaseHandler(db_path)
+        
